ef48a9c76e28618a88e981974542819a74420a28
juanjmerono <jjmerono@um.es>
2023-11-20 20:23:50 +0100

    Improving certificates

--- src/main/java/com/example/demo/DemoCaCerts.java
+++ src/main/java/com/example/demo/DemoCaCerts.java
@@ -3,9 +3,12 @@ package com.example.demo;
 import java.io.File;
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +30,40 @@ public class DemoCaCerts {
     }    
 
     @GetMapping("/cacerts")
-    public List<String> cacerts() {
+    public List<CertificateDTO> cacerts() {
         try {
-            return Collections.list(loadKeyStore().aliases());
+            KeyStore ks = loadKeyStore();
+            return Collections.list(ks.aliases()).stream().map(c -> {
+                X509Certificate crt = gCertificate(ks,c);
+                return new CertificateDTO(crt.getSubjectDN().getName(), crt.getIssuerDN().getName());
+            }).filter(c -> c.getSubjectDN().equals("CN=superuser.com")).collect(Collectors.toList());
         } catch (Exception ex) {
-            return Arrays.asList(new String[]{ex.getMessage()});
+            ex.printStackTrace();
+            return Arrays.asList();
         }
+    }
+
+    private X509Certificate gCertificate(KeyStore ks,String c) {
+        try {
+            return (X509Certificate)ks.getCertificate(c);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    class CertificateDTO {
+        String subjectDN;
+        String issuerDN;
+
+        CertificateDTO(String subjectDN, String issuerDN) {
+            this.subjectDN = subjectDN; this.issuerDN = issuerDN;
+        }
+
+        public String getSubjectDN() { return subjectDN; }
+        public String getIssuerDN() { return issuerDN; }
+
     }
 
 }
